8ce8fbe102c27ac6082d4a299031dec24da5441a
isaiahhaley0 <isaiah.haley@ucdenver.edu>
2021-03-17 17:19:34 -0600

    formatting update

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,6 +2,7 @@ function closebar(id) {
     document.getElementById('sidebar').classList.add('hidden')
     document.getElementById('openbutton').classList.remove('hidden')
     document.getElementById('closebutton').classList.add('hidden')
+    ocument.getElementById('feed').classList.add('left')
   }
   function closeright(id) {
     document.getElementById(id).style.width = 0;
@@ -13,4 +14,5 @@ function closebar(id) {
     document.getElementById('sidebar').classList.remove('hidden')
     document.getElementById('openbutton').classList.add('hidden')
     document.getElementById('closebutton').classList.remove('hidden')
+    document.getElementById('feed').classList.remove('left')
   }
